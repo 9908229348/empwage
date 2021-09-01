c27d99ed9acc3e1439158bae4f7a3a6d9ff4679d
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-01 13:47:32 +0530

    EmpWage-Function

---
+++ empfunc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+No_workingDays=20
+Max_Hr_In_Month=100
+totalempHrs=0
+totalworkingDays=0
+function getWorkingHrs() {
+case $1 in
+       $isPartTime)
+           empHrs=4
+          ;;
+       $isFullTime)
+            empHrs=8
+          ;;
+        *)
+           empHrs=0
+esac
+echo $empHrs
+}
+while [[ $totalempHrs -lt $Max_Hr_In_Month && $totalworkingDays -lt $No_workingDays ]]
+do
+((totalworkingDays++))
+empHrs="$( getWorkingHrs $((RANDOM%3)) )"
+totalempHrs=$(($totalempHrs+$empHrs))
+done
+totalsalary=$(($totalempHrs*$empRatePerHr))
