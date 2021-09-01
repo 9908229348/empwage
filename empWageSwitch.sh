5ed7fe9dd6e73ffa903c4da41b7b50d325db8e3d
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-01 12:49:17 +0530

    Emp wage-Switchcase

---
+++ empWageSwitch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+isPartTime=1
+isFullTime=2
+empRateperHr=20
+empCheck=$((RANDOM%3))
+case $empCheck in
+     $isPartTime)
+               empWorkingHr=4
+     ;;
+     $isFullTime)
+                empWorkingHr=8
+     ;;
+      *)
+             empWorkingHr=0
+esac
+salary=$(($empRateperHr*$empWorkingHr))
+
+
